2fdac8cb04f38a8731ee1eb432f2959703e01f54
Masayuki Higashino <msyk@hgsn.info>
2017-02-15 18:08:48 +0900

    support digest authentication

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -4,4 +4,16 @@ class Settings
     ENV['FISHING_CAT_REDIRECT_URL']
   end
 
+  def self.admin_realm
+    ENV['FISHING_CAT_ADMIN_REALM']
+  end
+
+  def self.admin_username
+    ENV['FISHING_CAT_ADMIN_USERNAME']
+  end
+
+  def self.admin_password
+    ENV['FISHING_CAT_ADMIN_PASSWORD']
+  end
+
 end
