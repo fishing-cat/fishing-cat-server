153f92c7124528286d3881685f25956a53a6b57f
Masayuki Higashino <msyk@hgsn.info>
2017-02-16 15:13:03 +0900

    update migration

--- db/migrate/20170215054721_create_events.rb
+++ db/migrate/20170215054721_create_events.rb
@@ -1,21 +1,22 @@
 class CreateEvents < ActiveRecord::Migration[5.0]
   def change
     create_table :events do |t|
-      t.string :fid
+      t.string :cid
       t.string :remote_ip
-      t.string :request_method
-      t.text :request_path
-      t.text :query_string
-      t.text :request_uri
-      t.text :http_user_agent
-      t.text :http_dnt
-      t.text :http_accept
-      t.text :http_accept_encoding
-      t.text :http_accept_language
-      t.text :params
+      t.text   :query_string
+      t.text   :request_method
+      t.text   :request_path
+      t.text   :request_uri
+      t.text   :http_user_agent
+      t.text   :http_dnt
+      t.text   :http_referer
+      t.text   :http_accept
+      t.text   :http_accept_encoding
+      t.text   :http_accept_language
+      t.text   :params
       t.timestamps
     end
-    %i(fid remote_ip request_method request_path).each do |column|
+    %i(cid remote_ip query_string request_method request_path request_uri http_user_agent http_dnt http_referer http_accept http_accept_encoding http_accept_language params).each do |column|
       add_index :events, column
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,34 @@
 ActiveRecord::Schema.define(version: 20170215054721) do
 
   create_table "events", force: :cascade do |t|
-    t.string   "fid"
+    t.string   "cid"
     t.string   "remote_ip"
-    t.string   "request_method"
-    t.text     "request_path"
     t.text     "query_string"
+    t.text     "request_method"
+    t.text     "request_path"
     t.text     "request_uri"
     t.text     "http_user_agent"
     t.text     "http_dnt"
+    t.text     "http_referer"
     t.text     "http_accept"
     t.text     "http_accept_encoding"
     t.text     "http_accept_language"
     t.text     "params"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
-    t.index ["fid"], name: "index_events_on_fid"
+    t.index ["cid"], name: "index_events_on_cid"
+    t.index ["http_accept"], name: "index_events_on_http_accept"
+    t.index ["http_accept_encoding"], name: "index_events_on_http_accept_encoding"
+    t.index ["http_accept_language"], name: "index_events_on_http_accept_language"
+    t.index ["http_dnt"], name: "index_events_on_http_dnt"
+    t.index ["http_referer"], name: "index_events_on_http_referer"
+    t.index ["http_user_agent"], name: "index_events_on_http_user_agent"
+    t.index ["params"], name: "index_events_on_params"
+    t.index ["query_string"], name: "index_events_on_query_string"
     t.index ["remote_ip"], name: "index_events_on_remote_ip"
     t.index ["request_method"], name: "index_events_on_request_method"
     t.index ["request_path"], name: "index_events_on_request_path"
+    t.index ["request_uri"], name: "index_events_on_request_uri"
   end
 
 end
