c3a749abf7bdd2548cc7ebcf8b466da63209b007
Masayuki Higashino <msyk@hgsn.info>
2017-02-20 00:42:44 +0900

    convert from js to coffee

---
+++ app/assets/javascripts/application.coffee
@@ -0,0 +1,6 @@
+#= require jquery
+#= require jquery_ujs
+#= require bootstrap-sprockets
+#= require bootstrap-table
+#= require extensions/bootstrap-table-mobile
+#= require_tree .
--- app/assets/javascripts/application.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require bootstrap-table
-//= require extensions/bootstrap-table-mobile
-//= require_tree .
