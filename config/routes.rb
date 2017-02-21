03060b81d7a1e02ef3218ce39b911e5de99517dd
Masayuki Higashino <msyk@hgsn.info>
2017-02-21 19:54:17 +0900

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,8 @@ Rails.application.routes.draw do
       resources :searches, only:[:index], format:'json'
     end
   end
-  get  '/result/:cid/:pid(/:fingerprint)', to:'fishing#viewed_result', format:false
-  get  '/result',                          to:'fishing#viewed_result', format:false
-  get  '/images/:cid/:pid(/:fingerprint)', to:'fishing#opened_email', format:false
-  get  '/images',                          to:'fishing#opened_email', format:false
-  get  '/:cid/:pid(/:fingerprint)',        to:'fishing#clicked_link', format:false
-  get  '/',                                to:'fishing#clicked_link', format:false
-  post '/:cid/:pid(/:fingerprint)',        to:'fishing#submitted_data', format:false
-  post '/',                                to:'fishing#submitted_data', format:false
+  get  '/result/(:cid/(:pid(/:fingerprint)))', to:'fishing#viewed_result',  format:false
+  get  '/images/(:cid/(:pid(/:fingerprint)))', to:'fishing#opened_email',   format:false
+  get  '(/(:cid/(:pid(/:fingerprint))))',      to:'fishing#clicked_link',   format:false
+  post '(/(:cid/(:pid(/:fingerprint))))',      to:'fishing#submitted_data', format:false
 end
