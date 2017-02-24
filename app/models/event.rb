d76f653ce781293a4db11df1b004b5f8cf35868c
Masayuki Higashino <msyk@hgsn.info>
2017-02-24 12:58:00 +0900

    remove records about privacy

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,26 +3,13 @@ class Event < ApplicationRecord
   include SearchCop
 
   search_scope :search do
-    attributes :id, :cid, :remote_ip, :query_string, :request_method, :request_path, :request_uri, :http_user_agent, :http_dnt, :http_referer, :http_accept, :http_accept_encoding, :http_accept_language, :params
+    attributes :id, :cid, :action
   end
 
   def self.log(params, request)
     event = {
       cid: params[:cid],
       pid: params[:pid],
-      remote_ip: request.remote_ip,
-      query_string: request.query_string,
-      request_method: request.request_method,
-      request_path: request.env['REQUEST_PATH'],
-      request_uri: request.env['REQUEST_URI'],
-      http_user_agent: request.user_agent,
-      http_dnt: request.env['HTTP_DNT'],
-      http_referer: request.env['HTTP_REFERER'],
-      http_accept: request.accept,
-      http_accept_encoding: request.accept_encoding,
-      http_accept_language: request.accept_language,
-      params: params.to_json,
-      controller: params[:controller],
       action: params[:action],
     }
     Event.create(event)
--- db/migrate/20170215054721_create_events.rb
+++ db/migrate/20170215054721_create_events.rb
@@ -3,23 +3,10 @@ class CreateEvents < ActiveRecord::Migration[5.0]
     create_table :events do |t|
       t.string :cid
       t.string :pid
-      t.string :remote_ip
-      t.text   :query_string
-      t.text   :request_method
-      t.text   :request_path
-      t.text   :request_uri
-      t.text   :http_user_agent
-      t.text   :http_dnt
-      t.text   :http_referer
-      t.text   :http_accept
-      t.text   :http_accept_encoding
-      t.text   :http_accept_language
-      t.text   :params
-      t.string :controller
       t.string :action
       t.timestamps
     end
-    %i(cid pid fingerprint remote_ip query_string request_method request_path request_uri http_user_agent http_dnt http_referer http_accept http_accept_encoding http_accept_language params controller action).each do |column|
+    %i(cid pid action).each do |column|
       add_index :events, column
     end
   end
