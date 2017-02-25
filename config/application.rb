2d1f2a84c5f66c97b50c889da77e93d39d1df2bf
Masayuki Higashino <msyk@hgsn.info>
2017-02-25 16:34:41 +0900

    rename app name from FishingCatServer to FishingCat::Server

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FishingCatServer
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+module FishingCat
+  module Server
+    class Application < Rails::Application
+      # Settings in config/environments/* take precedence over those specified here.
+      # Application configuration should go into files in config/initializers
+      # -- all .rb files in that directory are automatically loaded.
+    end
   end
 end
