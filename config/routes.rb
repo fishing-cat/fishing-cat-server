b581192f173d9ee05482714e4db258b4992ba578
Masayuki Higashino <msyk@hgsn.info>
2017-02-27 21:58:32 +0900

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
     resources :campaigns, param: :cid
     resources :events, only:[:index], format:/html|json/
   end
-  get  '/results/(:cid/(:pid))', to:'fishing#viewed_result',  format:false, as:'fishing_result'
-  get  '/images/(:cid/(:pid))',  to:'fishing#opened_email',   format:false
-  get  '/forms(/(:cid/(:pid)))', to:'fishing#clicked_link',   format:false
-  post '/forms(/(:cid/(:pid)))', to:'fishing#submitted_data', format:false
+  get '/results/(:cid/(:pid))', to:'fishing#viewed_result', format:false, as:'fishing_result'
+  get '/images/(:cid/(:pid))', to:'fishing#opened_email', format:false
+  get '/forms(/:cid(/:pid))', to:'fishing#clicked_link', format:false
+  post '/forms(/:cid(/:pid))', to:'fishing#submitted_data', format:false
   root to:'fishing#clicked_link', format:false
 end
