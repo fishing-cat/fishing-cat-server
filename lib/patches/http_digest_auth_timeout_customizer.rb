645ed82f6e15f54f93961af0cbd8bfe7fc52e960
Masayuki Higashino <msyk@hgsn.info>
2017-09-07 17:06:17 +0900

    Change timeout from 5 min. to 30 min. for http digest auth

--- lib/patches/http_digest_auth_timeout_customizer.rb
+++ lib/patches/http_digest_auth_timeout_customizer.rb
@@ -5,7 +5,7 @@ require 'action_controller/metal/http_authentication'
 module ActionController
   module HttpAuthentication
     module Digest
-      def validate_nonce(secret_key, request, value, seconds_to_timeout = 5 * 60)
+      def validate_nonce(secret_key, request, value, seconds_to_timeout = 30 * 60)
         return false if value.nil?
         t = ::Base64.decode64(value).split(":").first.to_i
         nonce(secret_key, t) == value && (t - Time.now.to_i).abs <= seconds_to_timeout
