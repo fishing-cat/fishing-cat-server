0ac2558eb5f7ed2bebb841fd09b633507193f4eb
Masayuki Higashino <higashino@tottori-u.ac.jp>
2023-09-03 04:16:41 +0900

    remove dependency of bootstrap-table

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Admin::EventsController < Admin::ApplicationController
 
   def index
@@ -7,6 +9,7 @@ class Admin::EventsController < Admin::ApplicationController
 
     respond_to do |format|
       format.html { render }
+      format.csv { send_events_csv(@events) }
       format.json {
         render json:{
           total:@events_count,
@@ -15,6 +18,38 @@ class Admin::EventsController < Admin::ApplicationController
       }
     end
 
+  end
+
+  def send_events_csv(events)
+    csv_data = CSV.generate do |csv|
+      column_names = %w(id cid pid action updated_at created_at fingerprint remote_ip query_string request_method request_path request_uri http_user_agent http_dnt http_referer http_accept http_accept_encoding http_accept_language params)
+      csv << column_names
+      events.each do |event|
+        column_values = [
+          event.id,
+          event.cid,
+          event.pid,
+          event.action,
+          event.updated_at,
+          event.created_at,
+          event.fingerprint,
+          event.remote_ip,
+          event.query_string,
+          event.request_method,
+          event.request_path,
+          event.request_uri,
+          event.http_user_agent,
+          event.http_dnt,
+          event.http_referer,
+          event.http_accept,
+          event.http_accept_encoding,
+          event.http_accept_language,
+          event.params,
+        ]
+        csv << column_values
+      end
+    end
+    send_data(csv_data, filename: "events-#{Time.now.iso8601}.csv", type: "text/csv", disposition: "inline")
   end
 
   def search
