20260dcc42500dc38c765c2943ec878f35d0873e
Masayuki Higashino <higashino@tottori-u.ac.jp>
2023-09-03 03:43:49 +0900

    Revert "fix: errors abound js"
    This reverts commit ddd3dc3f7671f3ce1a75f6bf4a859d1b5582ba3b.

--- app/javascript/application.js
+++
@@ -1,6 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails";
-import jquery from "jquery";
-window.jQuery = jquery;
-window.$ = jquery;
-import "bootstrap";
--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,9 @@ Bundler.require(*Rails.groups)
 module FishingCat
   module Server
     class Application < Rails::Application
+      # Initialize configuration defaults for originally generated Rails version.
+      config.load_defaults 5.1
+
       # Configuration for the application, engines, and railties goes here.
       #
       # These settings can be overridden in specific environments using the files
