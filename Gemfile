45195a0d88b67a413da5dc5117a7a11596dbdc39
Masayuki Higashino <higashino@tottori-u.ac.jp>
2023-09-02 21:41:50 +0900

    upgrade: ruby 2.4.2 -> 3.2.2 and rails 5.1.4 -> 5.1.7

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
-ruby '2.4.2'
+ruby '3.2.2'
 
-gem 'rails', '~> 5.1.4'
+gem 'rails', '~> 5.1.7'
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/application.rb
+++ config/application.rb
@@ -6,15 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FishingCat
-  module Server
-    class Application < Rails::Application
-      # Initialize configuration defaults for originally generated Rails version.
-      config.load_defaults 5.1
+module FishingCat::Server
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
 
-      # Settings in config/environments/* take precedence over those specified here.
-      # Application configuration should go into files in config/initializers
-      # -- all .rb files in that directory are automatically loaded.
-    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
