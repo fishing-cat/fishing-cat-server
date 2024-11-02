88de5c6602940723aa30b3e8bca25b394335a5f9
Masayuki Higashino <higashino@tottori-u.ac.jp>
2024-11-02 23:11:49 +0900

    add highlight.js

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,2 +1,9 @@
 //= require popper
 //= require bootstrap
+
+import hljs from "highlight.js";
+import hljsErb from "highlight.js/erb";
+hljs.registerLanguage("erb", hljsErb);
+document.addEventListener("turbo:load", () => {
+  hljs.highlightAll();
+});
