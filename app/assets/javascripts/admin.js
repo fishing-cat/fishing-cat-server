03f8dbb435f5eca75b7ab7e974b0e38f2110177e
Masayuki Higashino <higashino@tottori-u.ac.jp>
2024-11-03 19:05:05 +0900

    refactor js

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,8 +1,6 @@
-//= require popper
-//= require bootstrap
-
-import hljs from "highlight.js";
-import hljsErb from "highlight.js/erb";
+import * as bootstrap from "bootstrap";
+import hljs from "./highlight.js/highlight.min.js";
+import hljsErb from "./highlight.js/languages/erb.min.js";
 hljs.registerLanguage("erb", hljsErb);
 document.addEventListener("turbo:load", () => {
   hljs.highlightAll();
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,8 +2,5 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "bootstrap" # @5.3.3
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
-# highlight.js@11.10.0 https://github.com/highlightjs/highlight.js/tree/11.10.0
-pin "highlight.js", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/es/highlight.min.js"
-pin "highlight.js/erb", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/es/languages/erb.min.js"
+pin "bootstrap", to: "bootstrap.min.js", preload: true # via bootstrap.gem
+pin "@popperjs/core", to: "popper.js", preload: true # via bootstrap.gem
