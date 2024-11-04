5cf6eb22e4d1efa473496a343706538af855dd0e
Masayuki Higashino <higashino@tottori-u.ac.jp>
2024-11-05 01:03:38 +0900

    Fix bin/render-build.bash

--- bin/render-build.bash
+++ bin/render-build.bash
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# exit on error
 set -o errexit
 
 ./bin/bundle install
 ./bin/rails db:prepare
-./bin/rails assets:clean
 ./bin/rails assets:precompile
+./bin/rails assets:clean
